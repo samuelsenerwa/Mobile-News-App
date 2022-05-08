5dfc2e5381cec597e4b7d374cd6cc80687ba7bc1
samuel senerwa <samuelsenerwa@gmail.com>
2022-05-08 16:27:34 +0300

    first-commit

---
+++ app/src/main/java/com/example/newsapp/DetailsActivity.java
@@ -0,0 +1,39 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.newsapp.Models.NewsHeadlines;
+import com.squareup.picasso.Picasso;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    NewsHeadlines headlines;
+    TextView txt_title, txt_detail, txt_author, txt_content, txt_time;
+    ImageView img_news;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        txt_title = findViewById(R.id.text_detail_title);
+        txt_author = findViewById(R.id.text_detail_author);
+        txt_time = findViewById(R.id.text_detail_time);
+        txt_content = findViewById(R.id.text_detail_content);
+        txt_detail = findViewById(R.id.text_detail_detail);
+        img_news = findViewById(R.id.img_detail_news);
+
+        headlines = (NewsHeadlines) getIntent().getSerializableExtra("data");
+
+        txt_title.setText(headlines.getTitle());
+        txt_author.setText(headlines.getAuthor());
+        txt_time.setText(headlines.getPublishedAt());
+        txt_detail.setText(headlines.getDescription());
+        txt_content.setText(headlines.getContent());
+        Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+    }
+}
